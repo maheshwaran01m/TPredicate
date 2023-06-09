91bc7165a9d73bd5f3a5eae830f9c76ff6d25cbe
MAHESHWARAN <maheshwaran@eam360.com>
2023-06-09 10:26:51 +0530

    Updated with Examples

--- Tests/TPredicateTests/TPredicateTests.swift
+++ Tests/TPredicateTests/TPredicateTests.swift
@@ -2,10 +2,12 @@ import XCTest
 @testable import TPredicate
 
 final class TPredicateTests: XCTestCase {
+  
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(TPredicate().text, "Hello, World!")
+      XCTAssertNotNil(A().name)
+      XCTAssertNotEqual(A().name, B().job)
     }
 }
